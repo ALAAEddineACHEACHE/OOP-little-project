3a68843c60111e65bf2b6f1f9f70c42e430a5746
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-29 23:34:47 +0100

    Add files via upload

---
+++ fich1.js
@@ -0,0 +1,21 @@
+// function salutation(){
+//     console.log("bonjour tout le monde");
+// }
+// function surface(lar,lon){
+//     let s = lar*lon;
+//     console.log(s);
+// }
+const voiture ={
+    couleur:"rouge",
+    marque:"BMW",
+    matricule:"AB12332132",
+    rouler:function(){
+        console.log("Elle roule!");
+    },
+    arreter:stop()
+};
+function stop(){
+    console.log("ELLE STOPPE");
+}
+export default voiture;
+// export{salutation,surface};
